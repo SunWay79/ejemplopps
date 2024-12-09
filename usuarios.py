39dbcd8d5da285b75b126f36afdeb64be6ba3d0d
sunway <oscar@PortatilHP>
2024-12-09 16:12:40 +0100

    Segundo commit

--- usuarios.py
+++ usuarios.py
@@ -3,6 +3,6 @@ usuarios = {}
 
 def registrar_usuario(nombre, contrasena):
     if nombre in usuarios:
-        return "Usuario ya registrado"
+        return "Error: Usuario ya registrado"
     usuarios[nombre] = contrasena
     return "Usuario registrado"
